afa2bf88dc7733e42e4184e11d31c96ae1e80cac
mbeave <39030128+mbeave@users.noreply.github.com>
2023-01-06 20:05:18 -0600

    added form and some styling to title

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
 
 function App() {
+  const [url, setUrl] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`You entered: ${url}`);
+  }
+
   return (
-    <div className="App ">
-      <h1 className="text-3xl font-bold underline bg-lime-400">
-        Hello world!
-      </h1>
+    <div className=" flex-col">
+      <h1 className="text-center p-12 font-mono text-3xl font-bold text-white bg-neutral-500">GitHub Simplified</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Enter URL:{""}
+          <input type="url" value={url} onChange={e => setUrl(e.target.value)} />
+        </label>
+        <input type="submit"/>
+      </form>
+      <h5>URL: {url}</h5>
     </div>
   );
 };
